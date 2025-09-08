803b01856461549db3b9765488b612cd89f8be35
lIlIIlIlIIl <dptjd999@gmail.com>
2025-09-08 15:03:53 +0900

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,6 +1,10 @@
 def print_hello():
     print("Hello World")
 
+def print_KIT():
+print("Kumoh National
+Institute of Technology.")
 
-if__name__ == '__main__':
+if__name__ == '__main__':\
+    print_KIT()
     print_hello()
